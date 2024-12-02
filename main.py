a70b7f180a017788249d93fed8f65567edb3bee5
Daniel Sivertsen <daniel.sivertsen@outlook.com>
2024-12-02 09:55:24 +0000

    Clean up branches

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 def main():
-    def get_name():
-        #Prompt to get user name.
-        char_name = input("Enter character name:")
-        if len(char_name) <= 4:
-            raise Exception("The character name is too short.")
+    
+    new_character = Character(input("What is your name?\n"), input("What is your class?\n"), input("How much power do you have?\n"))
+    #print(new_character.name, new_character.spec, new_character.power)
+    welcome_message(new_character.name, new_character.spec, new_character.power)
+    # Testing character class.
+class Character:
+    def __init__(self, name, spec, power):
+        self.name = name
+        self.spec = spec
+        self.power = power
+    
+def welcome_message(name, spec, power):
+    max_attempts = 5
+    while max_attempts > 0:
+        if len(name) >= 4:
+            print("Sorry, the character name is too short. (Needs to be minimum 4 characters.)")
+            break
         else:
-            print(f"Welcome {char_name}!")
-    get_name()
+            print("ERROR")
+            max_attempts -= 1
+            if max_attempts == 0:
+                print("ERROR2")
+                continue
 main()
